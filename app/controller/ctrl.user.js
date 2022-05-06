a77dae31eecc2e774e4f8a8c4c5e2b1d2671a25b
Arnaud J <cutout.hoorays_0p@icloud.com>
2022-05-06 11:57:25 +0200

    login + JWT auth middleware implkemented ok

--- app/controller/ctrl.user.js
+++ app/controller/ctrl.user.js
@@ -10,13 +10,17 @@ const PASSWORD_REGEX = '';  // à définir
 const userController = {
 
   async register(req,res) {
+    // ToDo: Ajouter une vérification que le user n'existe pas déjà!
+    // Choix de l'élément discriminant (email?). + Check dans le controller? en BDD? les 2?
+    // Gérer l'ajout de la photo de profil, date de naissance et bio
+
     const { email, username, password, firstName, lastName, gender, birthdayDate, bio } = req.body;
 
     if (!email || !username || !password || !firstName || !lastName) {
       return res.status(401).json({ error: 'there is one missing field, please check again' });
     }
-    if (username.length >= 17 || username.length <= 5) {
-      return res.status(401).json({ error: 'wrong username (length must be: 6 - 16)' });
+    if (username.length >= 17 || username.length <= 4) {
+      return res.status(401).json({ error: 'wrong username (length must be: 5 - 16)' });
     }
     if (!EMAIL_REGEX.test(email)) {
       return res.status(401).json({ error: 'invalid email' });
@@ -38,28 +42,33 @@ const userController = {
     }
 
     const userInDb = await userDataMapper.findExistingUser(email);
-    // format du user (on a besoin d'un password, d'un id, et de savoir ou non si le user existe à partir de l'email)
-    console.log(userInDb)
+    const user = userInDb.rows[0];
 
-    if (result.row.length !== 1) {
-      // Vérifier si focntionne
+    // S'il n'y a pas une unique entrée pour cet email (par ex. 0: il n'existe pas), on renvoie une erreur
+    if (userInDb.rows.length !== 1) {
       return res.status(401).json({ error: 'user does not exist' });
     }
 
-    const passwordValidation = bcrypt.compare(password, userInDb.password);
+    // On utilise bcrypt pour comparer le mot de passe entré avec le hash stocké en DB (retourne un booléen)
+    const passwordValidation = await bcrypt.compare(password, user.hashedpassword);
     if (!passwordValidation) {
       return res.status(401).json({ error: 'incorrect password' });
     }
 
-    const generatedToken = jwt.sign({ userId: userInDb.id }, `${process.env.TOKEN_KEY}`, {
+    // On génère un token avec JWT (codé avec notre TOKEN_KEY), dans lequel on insère l'id du user
+    // sera utilisé pour authentifier toutes nos routes grâce à un middleware "auth"
+    const generatedToken = jwt.sign({ userId: user.id }, `${process.env.TOKEN_KEY}`, {
       expiresIn: sessionDuration,
     })
 
+    // On renvoie le Token
     res.status(200).json({
       auth: true,
+      userId: user.id,
       token: generatedToken
     });
   },
+
 
 
   // Controllers Not Yet coded:
--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -2,9 +2,12 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
   try {
+    // On récupère le token dans le header de la requête from front
     const token = req.headers["x-access-token"];
-    const decodedToken = jwt.verify(token, `${process.env.TOKEN_KEY}`);
+    // On le vérifie et récupère le userID qu'il contient (on l'avait créé lors du login)
+    const decodedToken = jwt.verify(token, `${process.env.TOKEN_KEY}`);;
     const userId = decodedToken.userId.toString();
+    // On stock le userID en res.locals, et on passe au controller suivant
     res.locals.user = userId;
     next();
   } catch {
