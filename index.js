bff4f537b5c8f7b357f30f757ea8eca5db8f9a77
Arnaud J <cutout.hoorays_0p@icloud.com>
2022-05-10 11:50:33 +0200

    adding access control

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ app.use(express.json());
 const userRoutes = require('./app/routes/route.user');
 const mediaRoutes = require('./app/routes/route.media');
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
 
 app.use('/api', userRoutes);
 app.use('/api/user', mediaRoutes);
