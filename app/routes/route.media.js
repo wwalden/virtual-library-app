6abd20488f6a18d5f029d49a966ca216463f3f6d
Hadrien-Rudich <94840435+Hadrien-Rudich@users.noreply.github.com>
2022-05-11 16:41:26 +0200

    user CRUD done

--- app/routes/route.media.js
+++ app/routes/route.media.js
@@ -8,9 +8,9 @@ const auth = require('../middleware/auth');
 // router.get("/:userid/collection", mediaController.getCollection)
 router.get("/:userid/:media", auth, mediaController.getCollection)
 router.get("/:userid/media/:mediaid", mediaController.getOneMedia) // ajouter le middleware auth
-router.post("/:userid/media/:mediaidid", mediaController.addOneMedia) // ajouter le middleware auth
-router.patch("/:userid/media/:mediaidid", mediaController.updateOneMedia) // ajouter le middleware auth
-router.delete("/:userid/media/:mediaidid", mediaController.deleteOneMedia) // ajouter le middleware auth
+router.post("/:userid/media/:mediaidid", mediaController.addOneReview) // ajouter le middleware auth
+router.patch("/:userid/media/:mediaidid", mediaController.updateOneReview) // ajouter le middleware auth
+router.delete("/:userid/media/:mediaidid", mediaController.deleteOneReview) // ajouter le middleware auth
 
 
 
--- app/routes/route.user.js
+++ app/routes/route.user.js
@@ -7,9 +7,9 @@ const auth = require('../middleware/auth');
 // User Routes
   router.post("/login", userController.login)
   router.post("/register", userController.register)
-  router.get("/profile/:id", userController.getProfile) // ajouter le middleware auth
-  router.patch("/profile/:id", userController.updateProfile) // ajouter le middleware auth
-  router.delete("/profile/:id", userController.deleteProfile) // ajouter le middleware auth
+  router.get("/profile/:id", auth, userController.getProfile)
+  router.patch("/profile/:id", auth, userController.updateProfile)
+  router.delete("/profile/:id", auth, userController.deleteProfile) 
 
 
 
