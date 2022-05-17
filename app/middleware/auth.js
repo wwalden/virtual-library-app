dec47dc93b622254e21f626e519a68d796c54f94
Arnaud J <cutout.hoorays_0p@icloud.com>
2022-05-17 11:50:09 +0200

    fixing auth middleware issue

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers["x-access-token"];
+    const token = req.headers.authorization;
     const decodedToken = jwt.verify(token, `${process.env.TOKEN_KEY}`);;
     const userId = decodedToken.userId.toString();
     // faire une recherche du user, être sur qu'il existe dans la DB pour un check de sécurité ?
