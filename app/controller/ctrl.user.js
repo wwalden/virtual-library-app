5c92597a65cc41a925b65c679bf1ac4a5aebe8fa
Arnaud J <cutout.hoorays_0p@icloud.com>
2022-05-18 12:26:23 +0200

    working on error status codes

--- app/controller/ctrl.user.js
+++ app/controller/ctrl.user.js
@@ -18,7 +18,6 @@ const userController = {
 
   async register(req,res) {
     const { email, username, password, firstName, lastName, gender, birthdayDate, bio } = req.body;
-
     if (!email || !username || !password || !firstName || !lastName) {
       return res.status(401).json({ error: 'there is one missing field, please check again' });
     }
@@ -31,40 +30,28 @@ const userController = {
     //if (!PASSWORD_REGEX.test(password)) {
       //return res.status(401).json({ error: 'invalid password' });
     //}
-
     const hash = await bcrypt.hash(req.body.password, 10);
     const newUser = await userDataMapper.registerNewUser(req.body, hash);
     res.send(newUser);
   },
-
 
   async login(req,res) {
     const { email, password } = req.body;
     if (!EMAIL_REGEX.test(email)) {
       return res.status(401).json({ error: 'email is not valid' });
     }
-
     const userInDb = await userDataMapper.findExistingUser(email);
     const user = userInDb.rows[0];
-
-    // S'il n'y a pas une unique entrée pour cet email (par ex. 0: il n'existe pas), on renvoie une erreur
     if (userInDb.rows.length !== 1) {
       return res.status(401).json({ error: 'user does not exist' });
     }
-
-    // On utilise bcrypt pour comparer le mot de passe entré avec le hash stocké en DB (retourne un booléen)
     const passwordValidation = await bcrypt.compare(password, user.hashedpassword);
     if (!passwordValidation) {
       return res.status(401).json({ error: 'incorrect password' });
     }
-
-    // On génère un token avec JWT (codé avec notre TOKEN_KEY), dans lequel on insère l'id du user
-    // sera utilisé pour authentifier toutes nos routes grâce à un middleware "auth"
     const generatedToken = jwt.sign({ userId: user.id }, `${process.env.TOKEN_KEY}`, {
       expiresIn: sessionDuration,
     })
-
-    // On renvoie le Token
     res.status(200).json({
       auth: true,
       userId: user.id,
@@ -72,40 +59,23 @@ const userController = {
     });
   },
 
-
-
   async getProfile(req,res) {
-
     const userId = res.locals.user
- 
     if(userId != req.params.id) {
-
-      return res.status(401).json({ error: 'incorrect userid' });
+      return res.status(403).json({ error: 'forbidden' });
     }
-
-      const results = await userDataMapper.getUserDetails(userId);
-      res.send(results);
-
-      
-  
-
-    
-    // check if req.params.id != userId + try catch
-    
+    const results = await userDataMapper.getUserDetails(userId);
+    res.send(results);
   },
 
   async updateProfile(req,res) {
     const userId = res.locals.user
     if(userId != req.params.id) {
-      res.redirect('/login');
+      return res.status(403).json({ error: 'forbidden' });
     }    
-    
     const { email, username, password, newpassword, firstName, lastName, gender, birthdayDate, bio, pictureurl } = req.body;
-    console.log(username);
-
     const userInDb = await userDataMapper.getUserDetails(userId);
     const user = userInDb.rows[0];
-
     if (username.length >= 17 || username.length <= 4) {
       return res.status(401).json({ error: 'invalid username (length must be: 5 - 16)' });
     }
@@ -116,11 +86,9 @@ const userController = {
     if (!passwordValidation) {
       return res.status(401).json({ error: 'invalid password' });
     }
-    
     //if (!PASSWORD_REGEX.test(password)) {
       //return res.status(401).json({ error: 'invalid password' });
     //}
-
     const newPassword = await bcrypt.hash(req.body.newpassword, 10);
     const updatedProfile = await userDataMapper.updateUser(req.body, newPassword, userId);
     res.send(updatedProfile);
@@ -129,11 +97,11 @@ const userController = {
   async deleteProfile(req,res) {
     const userId = res.locals.user
     if (userId != req.params.id) {
-      res.redirect('/login');
+      return res.status(403).json({ error: 'forbidden' });
     }
     await userDataMapper.deleteUser(userId);
     res.locals.user = 0
-    res.redirect('/');
+    return res.status(201).json({ error: 'user deleted' });
   }
 };
 
