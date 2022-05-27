ad7206c1ad2bb6e80b0e0c43bbfca570eef70cd7
Arnaud J <cutout.hoorays_0p@icloud.com>
2022-05-27 18:38:33 +0200

    fixing URI

--- app/model/dataClient.js
+++ app/model/dataClient.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const client = new Pool({
-  connectionString: 'postgres://hbjxklcdgtyfom:39a01cfee918fa7729f83dc6155c4dcb6cd6bb304f57fa405cdaaec69d4c986e@ec2-176-34-215-248.eu-west-1.compute.amazonaws.com:5432/d2s4rd97gne0v9',
+  connectionString: `${process.env.DB_URI}`,
   ssl:{
     rejectUnauthorized:false
   }
